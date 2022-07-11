54e71a4852bb1084f4a4844fb9e877dd04a437ba
KCH <doolysmile1@naver.com>
2022-07-11 14:14:18 +0900

    feat : 등록, 목록 추가

--- src/main/java/com/kch/ll/exam/App.java
+++ src/main/java/com/kch/ll/exam/App.java
@@ -1,13 +1,22 @@
 package com.kch.ll.exam;
 
 import java.io.FileInputStream;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public void run() {
         System.out.println("== 명언 SSG ==");
-        int i = 0;
+
         Scanner sc = new Scanner(System.in);
+
+        // 가장 마지막 명언글의 번호
+        List<WiseSaying> wiseSayings = new ArrayList<>();
+        int wiseSayingLastId = 0;
+
         outer:
         while (true) {
             System.out.printf("명령) ");
@@ -15,15 +24,26 @@ public class App {
 
             switch (cmd) {
                 case "등록":
-                    System.out.print("명언 : ");
-                    String wiseSaying = sc.nextLine().trim();
-                    System.out.print("명언 : ");
+                    System.out.printf("명언 : ");
+                    String content = sc.nextLine().trim();
+                    System.out.printf("작가 : ");
                     String author = sc.nextLine().trim();
-                    System.out.println(++i + "번 명언이 등록되었습니다.");
-                    break;
-                case "삭제":
+                    int id = ++wiseSayingLastId; // 명언 글 번호 증가
+
+                    WiseSaying wiseSaying = new WiseSaying(id, content, author);
+                    wiseSayings.add(wiseSaying);
+
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
                     break;
                 case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("-------------------");
+                    for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+                        WiseSaying wiseSaying_ = wiseSayings.get(i);
+                        System.out.printf("%d / %s / %s\n", wiseSaying_.id, wiseSaying_.content, wiseSaying_.author);
+                    }
+                    break;
+                case "삭제":
                     break;
                 case "종료":
                     break outer;
