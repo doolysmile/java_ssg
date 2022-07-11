893399fdbf64ba051d449b47f44c21fc6d0c95a2
KCH <doolysmile1@naver.com>
2022-07-11 14:13:42 +0900

    test : test 추가

--- src/test/java/com/kch/ll/exam/AppTest.java
+++ src/test/java/com/kch/ll/exam/AppTest.java
@@ -1,12 +1,12 @@
 package com.kch.ll.exam;
 
+
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test
@@ -16,7 +16,7 @@ public class AppTest {
     }
 
     @Test
-    public void 테스트_스캐너() {
+    public void 문자열을_스캐너의_입력으로_설정() {
         String input = """
                 등록
                 명언1
@@ -32,5 +32,23 @@ public class AppTest {
         assertEquals("등록", cmd);
         assertEquals("명언1", content);
         assertEquals("작가1", author);
+    }
+
+    @Test
+    public void 표준출력을_리다이렉션하여_결과를_문자열로_받기() throws IOException {
+        // 표준출력을 리다이렉션
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        System.out.println("안녕");
+
+        // 그 동안 System.out.println 으로 모아놨던 문장들을 받아옴
+        String rs = output.toString().trim();
+
+        // 표준출력을 원상복구
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        output.close();
+
+        assertEquals("안녕", rs);
     }
 }
