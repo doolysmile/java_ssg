14f5c38141183fd40b8e34e2e43ea21f9d793400
KCH <doolysmile1@naver.com>
2022-07-12 15:59:56 +0900

    feat : 명언 수정 기능 추가

--- src/main/java/com/kch/ll/exam/App.java
+++ src/main/java/com/kch/ll/exam/App.java
@@ -29,8 +29,8 @@ public class App {
                 case "등록":
                     write(rq);
                     break;
-                case "삭제":
-                    remove(rq);
+                case "수정":
+                    modify(rq);
                     break;
                 case "목록":
                     list(rq);
@@ -39,10 +39,36 @@ public class App {
                     break outer;
             }
         }
-
         sc.close();
     }
+    private void modify(Rq rq) {
+        // URL에 입력된 id 얻기
+        int paramId = rq.getIntParam("id", 0);
 
+        // URL에 입력된 id가 없다면 작업중지
+        if (paramId == 0) {
+            System.out.println("id를 입력해주세요.");
+            return;
+        }
+
+        // URL에 입력된 id에 해당하는 명언객체 찾기
+        WiseSaying foundWiseSaying = findById(paramId);
+
+        // 찾지 못했다면 중지
+        if (foundWiseSaying == null) {
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", paramId);
+            return;
+        }
+
+        System.out.printf("명언(기존) : %s\n", foundWiseSaying.content);
+        System.out.printf("명언 : ");
+        foundWiseSaying.content = sc.nextLine();
+        System.out.printf("작가(기존) : %s\n", foundWiseSaying.author);
+        System.out.printf("작가 : ");
+        foundWiseSaying.author = sc.nextLine();
+
+        System.out.printf("%d번 명언이 수정되었습니다.\n", paramId);
+    }
     private void list(Rq rq) {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("-------------------");
