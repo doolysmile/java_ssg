2a035c5b6b8ba534b36a1ccd25ec7bca6cc38631
KCH <doolysmile1@naver.com>
2022-07-17 23:28:23 +0900

    feat : 서비스 클래스

---
+++ src/main/java/com/kch/ll/exam/WiseSayingService.java
@@ -0,0 +1,35 @@
+package com.kch.ll.exam;
+
+import java.util.List;
+
+public class WiseSayingService {
+    WiseSayingRepository wiseSayingRepository;
+
+    public WiseSayingService() {
+        wiseSayingRepository = new WiseSayingRepository();
+    }
+
+    public WiseSaying write(String content, String author) {
+        return wiseSayingRepository.write(content, author);
+    }
+
+    public List<WiseSaying> findAll() {
+        return wiseSayingRepository.findAll();
+    }
+
+    public WiseSaying findById(int id) {
+        return wiseSayingRepository.findById(id);
+    }
+
+    public boolean modify(int id, String content, String author) {
+        return wiseSayingRepository.modify(id, content, author);
+    }
+
+    public boolean remove(int id) {
+        return wiseSayingRepository.remove(id);
+    }
+
+    public void saveFile() {
+        wiseSayingRepository.saveFile();
+    }
+}
