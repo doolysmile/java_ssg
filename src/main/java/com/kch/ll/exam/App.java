cf90252cef331d623b06783e7571145c41ee4091
KCH <doolysmile1@naver.com>
2022-07-17 23:29:19 +0900

    feat : 명령어 받는 부분 추가

--- src/main/java/com/kch/ll/exam/App.java
+++ src/main/java/com/kch/ll/exam/App.java
@@ -1,5 +1,7 @@
 package com.kch.ll.exam;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class App {
@@ -19,6 +21,39 @@ public class App {
     }
 
     public void run() {
+        System.out.println("== 명언 ==");
+        WiseSayingController wiseSayingController = new WiseSayingController(sc);
 
+        outer:
+        while(true) {
+            System.out.print("명언) ");
+            String command = sc.nextLine().trim();
+
+            RequestParams requestParams = new RequestParams(command);
+            switch (requestParams.getPath()) {
+                case "테스트":
+                    wiseSayingController.test();
+                    break;
+                case "등록":
+                    wiseSayingController.write();
+                    break;
+                case "목록":
+                    wiseSayingController.list();
+                    break;
+                case "삭제":
+                    wiseSayingController.remove(requestParams);
+                    break;
+                case "수정":
+                    wiseSayingController.modify(requestParams);
+                    break;
+                case "빌드":
+                    wiseSayingController.build();
+                    break;
+                case "종료":
+                    break outer;
+            }
+
+            
+        }
     }
 }
